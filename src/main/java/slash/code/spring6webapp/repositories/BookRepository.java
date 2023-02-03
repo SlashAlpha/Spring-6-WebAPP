4a56caf06aac3cc0cf679afeb8d432ee12ba9543
Clément Duprat <jobclement@hotmail.fr>
2023-02-03 11:46:56 +0100

    books & authors repositories and hibernate equity

---
+++ src/main/java/slash/code/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package slash.code.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import slash.code.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+
+
+}

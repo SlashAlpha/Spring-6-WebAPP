3d45f514131a54468b1f48605ed42c8269ecf65e
Clément Duprat <jobclement@hotmail.fr>
2023-02-03 11:16:57 +0100

    Spring 6 web App first commit Domain

---
+++ src/test/java/slash/code/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package slash.code.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

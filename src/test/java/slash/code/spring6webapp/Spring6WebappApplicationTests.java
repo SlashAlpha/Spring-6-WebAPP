9e92dea8042b2e9563b0bb6b7f1cb107df247531
Clément Duprat <jobclement@hotmail.fr>
2023-02-04 10:33:20 +0100

    context controller check with bean and autowiring

--- src/test/java/slash/code/spring6webapp/Spring6WebappApplicationTests.java
+++ src/test/java/slash/code/spring6webapp/Spring6WebappApplicationTests.java
@@ -1,10 +1,31 @@
 package slash.code.spring6webapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import slash.code.spring6webapp.controllers.AuthorController;
 
 @SpringBootTest
 class Spring6WebappApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    AuthorController controller;
+
+    @Test
+    void testAutowireOfController() {
+        controller.equals(applicationContext.getBean(AuthorController.class));
+        System.out.println(controller.sayHi());
+    }
+
+    @Test
+    void getControllerFromContext() {
+        AuthorController controller=applicationContext.getBean(AuthorController.class);
+        System.out.println( controller.sayHi());
+    }
 
     @Test
     void contextLoads() {
